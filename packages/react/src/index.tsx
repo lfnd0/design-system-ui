93cf835b6a58c2669f0fc51beab3921e2bbea83f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-02 18:44:44 -0300

    Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,12 @@
-import { colors } from '@design-system-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green500',
+  borderRadius: '$md',
+  padding: '$4',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.green300 }}>Hello world</h1>
+  return <Button>Click Me!</Button>
 }

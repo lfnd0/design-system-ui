af9bf9aad49a6435cebab947c99ffe6b6c7fd3fa
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-02 16:30:59 -0300

    Configurando pacote do React

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@design-system-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@design-system-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.green300 }}>Hello world</h1>
+}
